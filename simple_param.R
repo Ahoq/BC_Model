edb870e7f9df64b86ed1be19a45be6ea18e3c71b
Ahoq <adnanbinhoq@gmail.com>
2018-04-17 23:54:20 -0400

    R files added

---
+++ simple_param.R
@@ -0,0 +1,36 @@
+
+#simplifying non-dimensional parameters
+
+p1=(mu_B)/(mu_BSE)
+p2=(mu_TB)/(mu_BSE)
+p3=1
+p4=(mustar_SB)/(mu_BSE)
+p5=(mu_BB)/(mu_BSE)
+p6=(mu_BS)/(mu_BSE)
+p7=(a_D+gamma)/(mu_BSE)
+p8=1
+p9=(gamma)/(mu_BSE)
+p10=(lambda)/(mu_BSE)
+p11=(omega*lambda*Delta)/(epsilon*mu_BSE)
+p12=(lambda_m*Delta)/(mu_BSE)
+p13=(beta)/(mu_BSE)
+p14=(mustar_SB+a_EaS+r_am)/(mu_BSE)
+p15=(beta_E)/(mu_BSE)
+p16=(r_am)/(mu_BSE)
+p17=(a_Em)/(mu_BSE)
+p18=(a_EaT)/(mu_BSE)
+p19=(c*sigma)/(mu_BSE)
+p20=(r)/(mu_BSE)
+p21=(r)/(mu_BSE)
+p22=(d)/(mu_BSE)
+p23=(mu_TB+a_D)/(mu_BSE)
+p24=(mu_normal-mustar_SB)/(mu_BSE)
+p25=m/(mu_BSE*Delta)
+p26=(lambda_m*Delta)/(mu_BSE)
+#p26 = 0 # for yucks
+p27=(theta_shut)/(Delta)
+p28=(s)/(epsilon/sigma)^ell
+p29=(alpha)/(sigma)
+p30=(q)/(sigma)
+p31=ell
+
